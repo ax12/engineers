efa0b6532a0b8c1bbec5bf29af3e8c48aa87ec24
Max <2903186@gmail.com>
2019-09-02 21:55:06 +0500

    сделан вывод товаров в цикле по количеству массивов

--- parametr.php
+++ parametr.php
@@ -1,8 +1,36 @@
 <?php
+//bootstrap cols
+$col_lg_count = 4;
+$col_md_count = 6;
 //const
 $before_price = 'Стоимость в сутки';
-
+$after_price = 'руб/сутки';
+//указать количество массивов я хз как их посчитать средствами php
+$count_arrays = 10;
 //prices
-$item_name_1 = 'Генератор Hyundai HY3000F'; $price_1 = '1000'; $per = 'руб/сутки';
+$petrol_generator_1 = array(
+    "img" => 'HyundaiHY3000F.jpg',
+    "name" => "Генератор Hyundai HY3000F",
+    "price" => "от 1000",
+);
+
+$petrol_generator_2 = array(
+    "img" => 'champion-gg3300e.jpg',
+    "name" => "Генератор Champion GG3300E",
+    "price" => "",
+);
+
+$petrol_generator_3 = array(
+    "img" => 'champion-gg3300e.jpg',
+    "name" => "Генератор Champion GG3300E",
+    "price" => "",
+);
+
+$petrol_generator_4 = array(
+    "img" => 'champion-gg3300e.jpg',
+    "name" => "Генератор Champion GG3300E",
+    "price" => "",
+);
+
 
 ?>
